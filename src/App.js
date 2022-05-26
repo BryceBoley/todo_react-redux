edb109d861c973d45aae83d4242080dcab07c5cc
Bryce Boley <bryceboley1@gmail.com>
2022-05-26 11:44:57 -0600

    cleared input after saving todo, some input validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addTodo } from "./redux/actions/addTodo";
 import { useState } from "react";
+import "./App.css"
 
 
 
 function App() {
   const todos = useSelector((state) => state.todoReducer);
   const dispatch = useDispatch();
-
-  const renderedTodoList = todos.length > 0 ? todos.map((todo) =>
-    <li key={todo.id} todo={todo.todo} >{todo.todo}
-    <button>x</button></li>
-  ) : <></>
   const [inputTodo, setInputTodo] = useState("")
+  const renderedTodoList = todos.length > 0 ? todos.map((todo) =>
+    <li key={todo.id} todo={todo.todo}>
+      {todo.todo}
+    </li>
+  ) : <></>
+
+  const handleChange = e => setInputTodo(e.target.value);  
+
+  const handleKeyDown = e => {
+    if (e.which === 13 && inputTodo !== "") {
+      dispatch(addTodo({ todo: inputTodo, id: todos.length }))
+      setInputTodo("")
+    }
+  }
+
+  const handleOnClick = e => {
+    if (inputTodo !== "") {
+      dispatch(addTodo({ todo: inputTodo, id: todos.length }))
+      setInputTodo("")
+    } 
+  }
+  
   return (
     <div className="App">
       <ul>{renderedTodoList}</ul>
 
-      <input type="text" id="id" name="todo" value={inputTodo}
-        onChange={(e) => setInputTodo(e.target.value)}></input>
-      <button onClick={() => dispatch(addTodo({ todo: inputTodo, id: todos.length }))}>add todo</button>
+      <input 
+        type="text" 
+        id="id" 
+        name="todo"  
+        autoFocus={true} 
+        value={inputTodo}
+        onChange={handleChange} 
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleOnClick}>add todo</button>
       <hr></hr>
 
     </div>
